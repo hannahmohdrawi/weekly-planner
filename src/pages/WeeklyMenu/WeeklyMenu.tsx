4d7dbf89c32d52d433ddfa8d3f3aa540dba4a00f
hannahmohdrawi <hannahmohdrawi@hotmail.com>
2026-02-13 21:28:32 +0000

    Adding Weekly Menu table

--- src/pages/WeeklyMenu/WeeklyMenu.tsx
+++ src/pages/WeeklyMenu/WeeklyMenu.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { Container, Typography } from '@mui/material';
+import { Container, Typography, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
 import * as styles from './WeeklyMenu.styles';
+
+const daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
 const WeeklyMenu: React.FC = () => {
   return (
@@ -8,9 +10,34 @@ const WeeklyMenu: React.FC = () => {
       <Typography variant="h4" component="h1" sx={styles.title}>
         Weekly Menu
       </Typography>
-      <Typography variant="body1" color="text.secondary">
+      <Typography variant="body1" color="text.secondary" sx={{ mb: 3 }}>
         Your weekly meal plan will appear here.
       </Typography>
+
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell><strong>Day</strong></TableCell>
+              <TableCell><strong>Breakfast</strong></TableCell>
+              <TableCell><strong>Lunch</strong></TableCell>
+              <TableCell><strong>Dinner</strong></TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {daysOfWeek.map((day) => (
+              <TableRow key={day}>
+                <TableCell component="th" scope="row">
+                  {day}
+                </TableCell>
+                <TableCell>-</TableCell>
+                <TableCell>-</TableCell>
+                <TableCell>-</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </Container>
   );
 };
